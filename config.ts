291a56d1184a4e457ad8e855afc97dd1e236cd28
Ly <34117007+LyingZz@users.noreply.github.com>
2022-03-28 11:00:44 +0800

    Update config.ts

--- config.ts
+++ config.ts
@@ -17,13 +17,13 @@ export const home = {
    */
   projects: [
     {
-      cover: 'https://iph.href.lu/200x200?text=%E5%8D%A0%E4%BD%8D%E5%9B%BE&fg=666666&bg=cccccc',
+      cover: 'https://matrixlabs-resource.s3.ap-northeast-1.amazonaws.com/Test+img/Carbon+emissions/HomePage/Rectangle+5.png',
       title: '南京金融城超高层监测项目',
       link: '/'
     },
     {
-      cover: 'https://iph.href.lu/200x200?text=%E5%8D%A0%E4%BD%8D%E5%9B%BE&fg=666666&bg=cccccc',
-      title: '南京金融城超高层监测项目',
+      cover: 'https://matrixlabs-resource.s3.ap-northeast-1.amazonaws.com/Test+img/Carbon+emissions/HomePage/Rectangle+5.png',
+      title: '南京长江大桥项目',
       link: '/'
     },
     {
